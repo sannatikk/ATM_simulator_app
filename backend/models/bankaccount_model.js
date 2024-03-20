244a31a16eccf83d0ef63774e27716ffb7acf929
sannatikk <sanna.poritikkakoski@gmail.com>
2024-03-20 17:56:39 +0200

    bankaccount_model tiedosto perustoiminnot valmis

---
+++ backend/models/bankaccount_model.js
@@ -0,0 +1,28 @@
+const db = require('../database');
+
+// bankaccount JS object for queries in database table bank_account
+
+const bankaccount = {
+
+    getAllAccounts(callback){
+        return db.query("SELECT * FROM bank_account", callback);
+    },
+
+    getOneAccount(id, callback){
+        return db.query("SELECT * FROM bank_account WHERE id_account = ?", [id], callback);
+    },
+
+    addAccount(newAccount, callback){
+        return db.query("INSERT INTO bank_account (id_account, balance, credit_limit) VALUES (?, ?, ?)", [newAccount.id_account, newAccount.balance, newAccount.credit_limit], callback);
+    },
+
+    updateAccount(id, updateAccount, callback){
+        return db.query("UPDATE bank_account SET id_account = ?, balance = ?, credit_limit = ? WHERE id_account = ?", [updateAccount.id_account, updateAccount.balance, updateAccount.credit_limit], id, callback);
+    },
+
+    deleteAccount(id, callback){
+        return db.query("DELETE FROM bank_account WHERE id_account = ?", [id], callback);
+    }
+}
+
+module.exports = bankaccount;
